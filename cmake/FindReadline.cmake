9e1f510f41f1b2b213eef7610ee15cefc3d1a28d
裴扬威 <peiyangwei@weizoom.com>
2019-12-09 15:33:42 +0800

    On macos, use readline installed by brew

--- cmake/FindReadline.cmake
+++ cmake/FindReadline.cmake
@@ -1,53 +1,85 @@
-find_path(Readline_ROOT_DIR
-    NAMES include/readline/readline.h
-    PATHS /usr/local/opt/readline/ /opt/local/ /usr/local/ /usr/
+# from: https://git.tasktools.org/projects/TM/repos/task/browse/cmake/Modules/FindReadline.cmake
+# - Find the readline library
+# This module defines
+#  READLINE_INCLUDE_DIR, path to readline/readline.h, etc.
+#  READLINE_LIBRARIES, the libraries required to use READLINE.
+#  READLINE_FOUND, If false, do not try to use READLINE.
+# also defined, but not for general use are
+# READLINE_readline_LIBRARY, where to find the READLINE library.
+# READLINE_ncurses_LIBRARY, where to find the ncurses library [might not be defined]
+
+# Apple readline does not support readline hooks
+# So we look for another one by default
+IF (APPLE OR FREEBSD)
+  FIND_PATH (READLINE_INCLUDE_DIR NAMES readline/readline.h PATHS
+    /usr/local/opt/readline/include
+    /usr/include/
+    /sw/include
+    /opt/local/include
+    /opt/include
+    /usr/local/include
     NO_DEFAULT_PATH
-)
-find_path(Readline_INCLUDE_DIR
-    NAMES readline/readline.h
-    PATHS ${Readline_ROOT_DIR}/include
+    )
+ENDIF (APPLE OR FREEBSD)
+FIND_PATH (READLINE_INCLUDE_DIR NAMES readline/readline.h)
+
+
+# Apple readline does not support readline hooks
+# So we look for another one by default
+IF (APPLE OR FREEBSD)
+  FIND_LIBRARY (READLINE_readline_LIBRARY NAMES readline PATHS
+    /usr/local/opt/readline/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/lib
+    /usr/local/lib
     NO_DEFAULT_PATH
-)
+    )
+ENDIF (APPLE OR FREEBSD)
+FIND_LIBRARY (READLINE_readline_LIBRARY NAMES readline)
 
-find_library(Readline_LIBRARY
-    NAMES readline
-    PATHS ${Readline_ROOT_DIR}/lib
+# Sometimes readline really needs ncurses
+IF (APPLE OR FREEBSD)
+  FIND_LIBRARY (READLINE_ncurses_LIBRARY NAMES ncurses PATHS
+    /usr/local/opt/ncurses/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/lib
+    /usr/local/lib
+    /usr/lib
     NO_DEFAULT_PATH
-)
-if (NOT Readline_LIBRARY)
- message(FATAL_ERROR "Not found libreadline.a")
-endif ()
-find_library(History_LIBRARY
-    NAMES history
-    PATHS ${Readline_ROOT_DIR}/lib
-    NO_DEFAULT_PATH
-)
-if (NOT History_LIBRARY)
- message(FATAL_ERROR "Not found libhistory.a")
-endif ()
+    )
+ENDIF (APPLE OR FREEBSD)
+FIND_LIBRARY (READLINE_ncurses_LIBRARY NAMES ncurses)
 
-if(Readline_INCLUDE_DIR AND Readline_LIBRARY)
-  set(READLINE_FOUND TRUE)
-else(Readline_INCLUDE_DIR AND Readline_LIBRARY)
-  FIND_LIBRARY(Readline_LIBRARY NAMES readline PATHS Readline_ROOT_DIR)
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(Readline DEFAULT_MSG Readline_INCLUDE_DIR Readline_LIBRARY )
-  MARK_AS_ADVANCED(Readline_INCLUDE_DIR Readline_LIBRARY)
-endif(Readline_INCLUDE_DIR AND Readline_LIBRARY)
+MARK_AS_ADVANCED (
+  READLINE_INCLUDE_DIR
+  READLINE_readline_LIBRARY
+  READLINE_ncurses_LIBRARY
+  )
 
-mark_as_advanced(
-    Readline_ROOT_DIR
-    Readline_INCLUDE_DIR
-    Readline_LIBRARY
-)
+SET (READLINE_FOUND "NO" )
+IF (READLINE_INCLUDE_DIR)
+  IF (READLINE_readline_LIBRARY)
+    SET (READLINE_FOUND "YES" )
+    SET (READLINE_LIBRARIES
+      ${READLINE_readline_LIBRARY} 
+      )
 
-set(READLINE_INCLUDE_DIR ${Readline_INCLUDE_DIR})
-set(READLINE_LIBRARY ${Readline_LIBRARY} ${History_LIBRARY})
+    # some readline libraries depend on ncurses
+    IF (READLINE_ncurses_LIBRARY)
+      SET (READLINE_LIBRARIES ${READLINE_LIBRARIES} ${READLINE_ncurses_LIBRARY})
+    ENDIF (READLINE_ncurses_LIBRARY)
 
-find_package(Curses REQUIRED)
+  ENDIF (READLINE_readline_LIBRARY)
+ENDIF (READLINE_INCLUDE_DIR)
 
-if (READLINE_FOUND)
- set(READLINE_LIBRARIES ${READLINE_LIBRARY})
- list(APPEND READLINE_LIBRARIES ${CURSES_LIBRARIES})
- set(READLINE_INCLUDE_DIRS ${READLINE_INCLUDE_DIR} ${CURSES_INCLUDE_DIR})
-endif ()
+IF (READLINE_FOUND)
+  MESSAGE (STATUS "Found readline library")
+ELSE (READLINE_FOUND)
+  IF (READLINE_FIND_REQUIRED)
+    MESSAGE (FATAL_ERROR "Could not find readline -- please give some paths to CMake")
+  ENDIF (READLINE_FIND_REQUIRED)
+ENDIF (READLINE_FOUND)
